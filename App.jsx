9ffac15b5fa5678deaf30be4bdcc8beef6e63b75
Denia <DENIA.CASIMIRO@BARUCHMAIL.CUNY.EDU>
2025-11-23 20:22:47 -0500

    new change

---
+++ App.jsx
@@ -0,0 +1,23 @@
+const stub = null;
+export default stub;
+
+export default null;
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="car-container">
+        <div className="bubble">alert</div>
+        <img
+          src="/images/blue-car.png"
+          alt="Blue Car"
+          className="car-image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
